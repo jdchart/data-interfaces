79d330f8a81cafcc3ca4f2d38672b620715dcfaf
Jacob Hart <jacob@uc12825.uni.lux>
2025-12-10 15:29:51 +0100

    initial

---
+++ _app/immutable/chunks/CxBL3-dk.js
@@ -0,0 +1,1 @@
+import{j as a}from"./M02_4qWR.js";a();
---
+++ _app/immutable/nodes/2.Dldxt22D.js
@@ -0,0 +1,1 @@
+import{f as o,a}from"../chunks/6qFRefbA.js";import"../chunks/CxBL3-dk.js";import{n as r}from"../chunks/M02_4qWR.js";var m=o('<h1>Welcome to SvelteKit</h1> <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>',1);function s(t){var e=m();r(2),a(t,e)}export{s as component};
