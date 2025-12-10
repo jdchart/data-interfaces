90d5f005abb2ab354b39512a9fe77d5c8afffbf0
Jacob Hart <jacob@uc12825.uni.lux>
2025-12-10 15:35:50 +0100

    fallback

--- svelte.config.js
+++ svelte.config.js
@@ -8,7 +8,9 @@ const config = {
     // preprocess: vitePreprocess(),
  
     kit: {
-        adapter: adapter(),
+        adapter: adapter({
+			fallback: 'index.html' // may differ from host to host
+		}),
 		paths : {
 			base: process.env.NODE_ENV === 'production' ? '/sveltekit-github-pages' : '',
 		}
