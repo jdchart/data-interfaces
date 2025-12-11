7d626b5f5a0b6aa525e473f1dc1dc8fdb7671120
Jacob Hart <jacob@uc12825.uni.lux>
2025-12-11 11:11:23 +0100

    param update

--- src/routes/tests/url-params/+page.svelte
+++ src/routes/tests/url-params/+page.svelte
@@ -1,9 +1,17 @@
 <script>
-    import { page } from '$app/stores';
-    import { get_url_params } from '$lib/scripts/urls';
-    $: params = Object.fromEntries($page.url.searchParams.entries());
+    // import { page } from '$app/stores';
+    // import { get_url_params } from '$lib/scripts/urls';
+    // $: params = Object.fromEntries($page.url.searchParams.entries());
 
+    
+    
+
+    import { page } from '$app/stores';
+
+    $: params = Object.fromEntries($page.url.searchParams.entries());
     console.log(params);
 </script>
+
+<pre>{JSON.stringify(params, null, 2)}</pre>
 
 <h1>URL PARAM TEST</h1>
