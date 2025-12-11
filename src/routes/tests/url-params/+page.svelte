c821ace52a0683f86f2be5c44e74eaab370d629c
Jacob Hart <jacob@uc12825.uni.lux>
2025-12-11 11:16:13 +0100

    vsdfb

--- src/routes/tests/url-params/+page.svelte
+++ src/routes/tests/url-params/+page.svelte
@@ -1,17 +1,10 @@
 <script>
-    // import { page } from '$app/stores';
-    // import { get_url_params } from '$lib/scripts/urls';
-    // $: params = Object.fromEntries($page.url.searchParams.entries());
-
-    
-    
-
     import { page } from '$app/stores';
 
+    // automatically update when query params change
     $: params = Object.fromEntries($page.url.searchParams.entries());
-    console.log(params);
+
+    console.log(params)
 </script>
 
 <pre>{JSON.stringify(params, null, 2)}</pre>
-
-<h1>URL PARAM TEST</h1>
