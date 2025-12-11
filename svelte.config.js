6d51cc1a8fe1f4c511047066ff442cebd781e82d
Jacob Hart <jacob@uc12825.uni.lux>
2025-12-11 10:30:10 +0100

    fallback

--- svelte.config.js
+++ svelte.config.js
@@ -5,7 +5,9 @@ export default {
 	prerender: {
 		entries: ['*']
 	},
-    adapter: adapter(),
+	adapter: adapter({
+			fallback: 'index.html' // may differ from host to host
+	}),
     paths: {
       base: process.env.NODE_ENV === "production" ? "/data-interfaces" : ""
     }
